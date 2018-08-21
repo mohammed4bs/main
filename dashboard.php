c2c774b749e00c20b56b83172c1860863c564ed6
Mohammed Helmy <mohammed4bs@gmail.com>
2018-08-21 11:03:05 +1100

    search form for clients

--- dashboard.php
+++ dashboard.php
@@ -1,17 +1,31 @@
 <?php
     session_start();
-  
-    
+   
     if(isset($_SESSION['username'])) {
         $pageTitle = "لوحة التحكم";
         include 'init.php';
         include $tbl . 'header.php';
+        if (isset($_GET['client'])) {
+            $client = $_GET['client'];
+            $stmt = $db->prepare('SELECT client_name FROM clients WHERE client_name = ?');
+            $stmt->execute(array($client));
+            $row = $stmt->rowCount();
+            
+            if ($row > 0 ) {
+                echo "Found";
+                
+            }
+            
+        }
+        
         
 
     ?>
         <div class="container-fluid">
             <h2> مرحبا,  <?php echo $_SESSION['username']; ?></h2>
         </div>
+        
+
     <?php
     } else {
         header('Location: index.php');
--- includes/nav.php
+++ includes/nav.php
@@ -27,8 +27,8 @@
                 <a class="nav-link disabled" href="#">Disabled</a>
             </li> -->
         </ul>
-        <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="search" placeholder="بحث عن عميل" aria-label="Search">
+        <form class="form-inline my-2 my-lg-0" action="dashboard.php" method="GET">
+            <input class="form-control mr-sm-2" type="text" name="client" placeholder="بحث عن عميل" aria-label="Search">
         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">بحث</button>
         </form>
         <div class="dropdown">
