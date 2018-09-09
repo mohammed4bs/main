347bdcd7a2531e0c28bb85adf78110f1acf60ba8
Mohammed Helmy <mohammed4bs@gmail.com>
2018-09-09 21:48:11 +0200

    Add dropdown solution for units

---
+++ ajaxData.php
@@ -0,0 +1,46 @@
+<?php 
+
+session_start();
+// Check if there's a session with the user name============================
+if (isset($_SESSION['username'])) {
+    include 'init.php';
+
+    if(!empty($_POST["company_id"])){
+        //Fetch all state data
+        $stmt = $db->prepare("SELECT * FROM reefs WHERE company_id = ? ORDER BY reef_id ASC");
+        $stmt->execute(array($_POST['company_id']));
+        $reefs = $stmt->fetchAll();
+        //Count total number of rows
+        $rowCount = $stmt->rowCount();
+        echo $rowCount;
+        //State option list
+        if($rowCount > 0){
+            echo '<option value="">اختر الريف</option>';
+            foreach ($reefs as $reef) {
+                echo '<option value="'.$reef['reef_id'].'">'.$reef['reef_name'].'</option>';
+            }    
+        }else{
+            echo '<option value="">لا يوجد ريف</option>';
+        }
+    }elseif(!empty($_POST["reef_id"])){
+        //Fetch all city data
+        $stmt = $db->prepare("SELECT * FROM units WHERE reef_id = ? ORDER BY unit_id ASC");
+        $stmt->execute(array($_POST['reef_id']));
+        $units = $stmt->fetchAll();
+        //Count total number of rows
+        $rowCount = $stmt->rowCount();
+        
+        //City option list
+        if($rowCount > 0){
+            echo '<option value="">اختر القطعة</option>';
+            foreach ($units as $unit) {
+                echo '<option value="'.$unit['unit_id'].'">'.$unit['unit_name'].'</option>';
+            }
+        }else{
+            echo '<option value="">لا توجد قطع </option>';
+        }
+    }
+} else {
+    echo 'Worng';
+}
+?>
--- layout/js/main.js
+++ layout/js/main.js
@@ -4,14 +4,56 @@ $(function () {
     // Delete Confimation
 
     $('.confirm').click(function () {
-        return confirm('هل تريد حذف هذا العميل؟');
+        return confirm('هل تريد حذف هذا السجل؟');
     });
 
-
+    $('#comp').on('change',function(){
+        var companyID = $(this).val();
+        if(companyID){
+            $.ajax({
+                type:'POST',
+                url:'ajaxData.php',
+                data:'company_id='+companyID,
+                success:function(html){
+                    $('#reef').html(html);
+                    $('#unit').html('<option value="">اختر ريف أولا</option>'); 
+                }
+            }); 
+        }else{
+            $('#reef').html('<option value="">اختر شركة أولا</option>');
+            $('#unit').html('<option value="">اختر ريف أولا</option>'); 
+        }
+        
+    });
     
-
-
-
-
+    $('#reef').on('change',function(){
+        var reefID = $(this).val();
+        if(reefID){
+            $.ajax({
+                type:'POST',
+                url:'ajaxData.php',
+                data:'reef_id='+reefID,
+                success:function(html){
+                    $('#unit').html(html);
+                }
+            }); 
+        }else{
+            $('#unit').html('<option value="">Select state first</option>'); 
+        }
+    });
 
 });
+
+
+function filter() {
+    var keyword = document.getElementById("search").value;
+    var fleet = document.getElementById("select");
+    for (var i = 0; i < fleet.length; i++) {
+        var txt = fleet.options[i].text;
+        if (txt.substring(0, keyword.length).toLowerCase() !== keyword.toLowerCase() && keyword.trim() !== "") {
+            fleet.options[i].style.display = 'none';
+        } else {
+            fleet.options[i].style.display = 'list-item';
+        }
+    }
+}
