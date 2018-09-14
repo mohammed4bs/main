1d2d32919f6cddf7e0f108cb334388fc00a6b6c4
Mohammed Helmy <mohammed4bs@gmail.com>
2018-09-15 01:22:34 +0200

    just another test

--- test.php
+++ test.php
@@ -1,7 +1,11 @@
 <?php
+$fee = 500;
+$start_date = date('i');
+$end_date = date('i') + 1;
+if (date('i') == $end_date) {
+    $fee += 500;
+    echo $fee;
+}
+echo '<br> ' . $start_date . '   <br>          ' .$end_date . '<br>';
 
-    $now = time(); // Use current time.
-
-$current_month = date('n', $now);
-echo $current_month;
 ?>
