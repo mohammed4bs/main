8e5f620558f7d7e8063d5d252ef9c8f18e147b6c
Mohammed Helmy <mohammed4bs@gmail.com>
2018-09-27 23:42:49 +0200

    Add local bootstrap.js

--- includes/footer.php
+++ includes/footer.php
@@ -7,7 +7,7 @@
   integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
   crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://cdn.rtlcss.com/bootstrap/v4.0.0/js/bootstrap.min.js" integrity="sha384-54+cucJ4QbVb99v8dcttx/0JRx4FHMmhOWi4W+xrXpKcsKQodCBwAvu3xxkZAwsH" crossorigin="anonymous"></script>
+    <script src="layout/js/bootstrap.min.js"  crossorigin="anonymous"></script>
     <script src="layout/js/main.js"></script>
   </body>
 </html>
