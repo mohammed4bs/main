0382ed11ae373cb9192d21f708680a02e6159182
Mohammed Helmy <mohammed4bs@gmail.com>
2018-10-20 18:18:44 +0200

    back to month

--- init.php
+++ init.php
@@ -25,18 +25,18 @@ foreach ($rows as $row) {
     $expDate = $row['start_date'];
     //echo '<h3>Expiy Date is ' . $expDate . '</h3><br>';
     if ($expDate < $curDate) {
-        // $d = (int)abs((strtotime($curDate) - strtotime($expDate))/(60*60*24*30));; == Month
-        $d = (int)abs((strtotime($curDate) - strtotime($expDate))/(60*60*24));; // days
-        //echo '<h3> diffrence is ' . $d . '</h3><br>';
+         $d = (int)abs((strtotime($curDate) - strtotime($expDate))/(60*60*24*30));; //== Month
+        //$d = (int)abs((strtotime($curDate) - strtotime($expDate))/(60*60*24));; // days
+        echo '<h3> diffrence is ' . $d . '</h3><br>';
         $m = $d * $row['total_space'] * $row['maint_fee']; 
         //echo '<h3> result is ' . $m . '</h3><br>';
         $blnc = $row['balance'];
         //$m = $m + $blnc;
         
         $start_date_ = $curDate;   //date("Y-m-d", strtotime(date("Y-m-d", strtotime($curDate)). "next day"));   //date('Y-m-01');
-        $end_date_ = date("Y-m-d", strtotime(date("Y-m-d", strtotime($start_date_)). "next day"));
+        $end_date_ = date("Y-m-d", strtotime(date("Y-m-d", strtotime($start_date_)). "next month"));
         //echo '<h3> Next Start Date is ' . $start_date_ . '</h3><br>';
-        //echo '<h3> Next End Date is ' . $end_date_ . '</h3><br>';
+        echo '<h3> Next End Date is ' . $end_date_ . '</h3><br>';
         $updateBalance = $db->prepare('UPDATE maint SET balance = :zblnce, start_date = :sdt, end_date = :edt WHERE contract_id = :zid');
         $updateBalance->execute(array(
             ':zblnce' => $m + $blnc,
